02db8a037f635eb22579d942c547c7eafc74a8b7
David Cai <DavidCai1246@hotmail.com>
2021-04-27 23:11:21 -0700

    pass in wrong variables

--- helper/snag.py
+++ helper/snag.py
@@ -4,8 +4,9 @@ from sendEmail import send_email
 import time
 
 def snagCourse(courseSubject, courseNumber, courseSection, email):
-	r = requests.get('https://api.ubccourses.com/sectionInfo/{}/{}/{}?realtime=1'.format(courseSubject, courseNumber, courseSection))
-
+	url = 'https://api.ubccourses.com/sectionInfo/{}/{}/{}?realtime=1'.format(courseSubject, courseNumber, courseSection)
+	r = requests.get(url)
+	print("Calling: ".format(url))
 	r_dict = r.json()
 
 	if(r_dict['general_seats_remaining'] > 0):
@@ -17,7 +18,7 @@ def snagCourse(courseSubject, courseNumber, courseSection, email):
 
 while(True):
 	print("Checking course status:")
-	result = snagCourse(os.environ["COURSESUBJECT"], os.environ["COURSESUBJECT"], os.environ["COURSENUMBER"], os.environ["COURSESECTION"])
+	result = snagCourse(os.environ["COURSESUBJECT"], os.environ["COURSENUMBER"], os.environ["COURSESECTION"], os.environ["EMAIL"])
 	if(result == True):
 		break;
 	time.sleep(3600)
