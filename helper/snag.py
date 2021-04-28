eef58f8f9280c61f5433a1e1d86d94da35089935
David Cai <DavidCai1246@hotmail.com>
2021-04-27 23:04:13 -0700

    add loop that runs snagCourse per hour

--- helper/snag.py
+++ helper/snag.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from sendEmail import send_email
+import time
 
 def snagCourse(courseSubject, courseNumber, courseSection, email):
 	r = requests.get('https://api.ubccourses.com/sectionInfo/{}/{}/{}?realtime=1'.format(courseSubject, courseNumber, courseSection))
@@ -10,8 +11,15 @@ def snagCourse(courseSubject, courseNumber, courseSection, email):
 	if(r_dict['general_seats_remaining'] > 0):
 		print("Sending email")
 		send_email("Test", "Your course {}{} {} has an open spot!".format(courseSubject, courseNumber, courseSection), email)
+		return True;
+	else:
+		return False;
 
-print(os.environ["EMAIL"])
-print(os.environ["COURSESUBJECT"])
-print(os.environ["COURSENUMBER"])
-print(os.environ["COURSESECTION"])
+while(True):
+	print("Checking course status:")
+	result = snagCourse(os.environ["COURSESUBJECT"], os.environ["COURSESUBJECT"], os.environ["COURSENUMBER"], os.environ["COURSESECTION"])
+	if(result == True):
+		break;
+	time.sleep(3600)
+
+print("Done")
