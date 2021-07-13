a9c8bf8608d3e976ff5c898b78a46f051d227e78
DavidCai1246 <45836644+DavidCai1246@users.noreply.github.com>
2021-07-13 12:13:56 -0700

    Update snag to check for total_seats_remaining

--- helper/snag.py
+++ helper/snag.py
@@ -6,9 +6,9 @@ def snagCourse(courseSubject, courseNumber, courseSection, email):
 	r = requests.get(url)
 	print("Calling: {}".format(url))
 	r_dict = r.json()
-	print(r_dict)
+	print("The current total seats remaining is: {}", r_dict['total_seats_remaining'])
 
-	if(r_dict['general_seats_remaining'] > 0):
+	if(r_dict['total_seats_remaining'] > 0):
 		print("Sending email")
 		send_email("Test", "Your course {}{} {} has an open spot!".format(courseSubject, courseNumber, courseSection), email)
 		return True;
